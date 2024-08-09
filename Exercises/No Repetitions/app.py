ba845df03470c589eeaad77b07f0790c597a7224
Dimitrios Kitsikidis <31593501+dimitriosxmi@users.noreply.github.com>
2024-08-09 12:25:41 +0200

    Complete exercise [No Repetitions].

--- Exercises/No Repetitions/app.py
+++ Exercises/No Repetitions/app.py
@@ -1,24 +1,23 @@
 from Exercises.functions import list_generator, string_generator
 
-my_list = list_generator(10, str_gen_settings=[5, ["penis"]])
-print(my_list)
+string_list = list_generator(str_gen_settings=[10])
 
-s = string_generator(5, ["abc"])
-s_length = len(s)
-temp_s = ""
-uniques = 0
-uniques_count = 0
+for s in string_list:
+    # s = string_generator(5, ["abc"])
+    s_length = len(s)
+    s_substring = ""
+    uniques_count = 0
+    counter = 0
 
-for char in s:
-    # print(char)
-    if char not in temp_s:
-        temp_s += char
-        uniques_count += 1
-        if uniques < uniques_count:
-            uniques = uniques_count
-    else:
-        temp_s = char
-        uniques_count = 1
+    for char in s:
+        # print(char)
+        if char not in s_substring:
+            s_substring += char
+            counter += 1
+            if uniques_count < counter:
+                uniques_count = counter
+        else:
+            s_substring = char
+            counter = 1
 
-print(s)
-# print(uniques)
+    print("s: [" + s + "] uniques_count: [" + str(uniques_count) + "]")
